cd7c02964a1ca11c16cf5e583b4f0311141f2102
Amin0Mohamed <aminelcaptain20@gmail.com>
2024-11-11 23:26:06 +0200

    first commit

---
+++ Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+	class Car
+	{
+
+		double Price;
+		string Number;
+		string Color;
+		double weight;
+		bool Transmission;
+
+
+		void MoveForward()
+		{
+
+		}
+
+        void Stop()
+        {
+
+        }
+
+    }
+}
